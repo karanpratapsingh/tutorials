4849b4a1b86c8891b41e94d9c2e6c1c47c48fcec
Karan Pratap Singh <synester.gates4@gmail.com>
2021-06-22 15:05:07 +0530

    feat: webpack with esbuild

---
+++ 1. webpack with esbuild/webpack.config.js
@@ -0,0 +1,63 @@
+// External modules
+const path = require('path');
+
+// Webpack plugins
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+// Environment config
+const isDevelopment = process.env.NODE_ENV !== 'production';
+const mode = isDevelopment ? 'development' : 'production';
+
+// Bundle config options
+const BUNDLE = {
+  entry: './index.ts',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+module.exports = {
+  mode,
+  target: 'node',
+  entry: BUNDLE.entry,
+  stats: 'errors-only',
+  module: getLoaders(),
+  plugins: getPlugins(),
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json']
+  },
+  output: BUNDLE.output
+};
+
+/**
+ * Loaders used by the application.
+ */
+function getLoaders() {
+  const esbuild = {
+    test: /\.(js|jsx|ts|tsx)?$/,
+    loader: 'esbuild-loader',
+    options: {
+      loader: 'tsx',
+      target: 'es2015'
+    },
+    exclude: /node_modules/
+  };
+
+  const loaders = {
+    rules: [esbuild]
+  };
+
+  return loaders;
+}
+
+/**
+ * Plugins
+ */
+function getPlugins() {
+  const nodemon = new NodemonPlugin();
+  const tsChecker = new ForkTsCheckerWebpackPlugin();
+
+  return [tsChecker, nodemon];
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Tutorials
+
+This repository contains code for all the tutorials I write about:
+
+1. webpack with esbuild
