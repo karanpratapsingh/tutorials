113d8fab41e6c0fbaf6cc6f7694bfd069d0d7a6f
Karan Pratap Singh <synester.gates4@gmail.com>
2021-08-16 14:53:15 +0530

    update(go-crud): pkg mocks

---
+++ go/crud/pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "github.com/tutorials/go/crud/pkg/models"
+
+var Books = []models.Book{
+	{
+		Id:     1,
+		Title:  "Golang",
+		Author: "Gopher",
+		Desc:   "A book for Go",
+	},
+}
