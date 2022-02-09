fef046d0d7f5bcace3c8174cb167fede1d68c0fe
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-09 21:08:14 +0530

    update: types

--- go/nats-gql/graph/resolver.go
+++ go/nats-gql/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/nats-io/nats.go"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	Nats *nats.Conn
+}
