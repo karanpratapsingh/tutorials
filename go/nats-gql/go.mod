64403ecf73a5dfeac2efb9a54c67739f58b24451
Karan Pratap Singh <synester.gates4@gmail.com>
2022-02-09 21:06:55 +0530

    feat: gql nats init

---
+++ go/nats-gql/go.mod
@@ -0,0 +1,24 @@
+module example
+
+go 1.17
+
+require (
+	github.com/99designs/gqlgen v0.16.0
+	github.com/vektah/gqlparser/v2 v2.2.0
+)
+
+require (
+	github.com/agnivade/levenshtein v1.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/matryer/moq v0.2.3 // indirect
+	github.com/mitchellh/mapstructure v1.2.3 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
+)
---
+++ go/nats-gql/graph/schema.resolvers.go
@@ -0,0 +1,27 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"example/graph/generated"
+	"fmt"
+)
+
+func (r *queryResolver) Payload(ctx context.Context) (*string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *subscriptionResolver) Payload(ctx context.Context) (<-chan *string, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+// Subscription returns generated.SubscriptionResolver implementation.
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
+
+type queryResolver struct{ *Resolver }
+type subscriptionResolver struct{ *Resolver }
